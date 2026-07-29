809592821795c7ce18b41abccaeee36e956fa0ab
Cirrest <a1042424422@163.com>
2026-07-29 21:39:03 +0800

    V2.0.1 update

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -17,12 +17,10 @@ MAGISKPOLICY="/data/adb/magisk/magiskpolicy"
 chcon u:object_r:vendor_configs_file:s0 "$MODDIR/system/vendor/etc/media_codecs.xml"
 chcon u:object_r:vendor_configs_file:s0 "$MODDIR/system/vendor/etc/media_codecs_c2_dolby_audio.xml"
 chcon u:object_r:vendor_configs_file:s0 "$MODDIR/system/vendor/etc/audio_effects.xml"
+chcon u:object_r:vendor_configs_file:s0 \
+    "$MODDIR/system/vendor/odm/etc/dolby/multimedia_dolby_dax_default.xml"
 chcon -R u:object_r:vendor_file:s0 "$MODDIR/system/vendor/lib"
 chcon -R u:object_r:vendor_file:s0 "$MODDIR/system/vendor/lib64"
-
-if [ ! -f /odm/.md_ph_001_dolby_probe ]; then
-    mount --bind "$MODDIR/payload/odm" /odm
-fi
 
 mkdir -p /data/vendor/dolby
 chown media:media /data/vendor/dolby
